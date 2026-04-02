b4ddfcfdbba6820d8668f2fef02b1d75a526637b
__hpatrik__ <heckopatrik05@gmail.com>
2026-04-02 12:11:00 +0200

    SUPER!

--- BooksApp/App/Controllers/BookController.php
+++ BooksApp/App/Controllers/BookController.php
@@ -86,16 +86,21 @@ class BookController {
     // --- Pomocné metody pro systém notifikací ---
     // (V reálném projektu by tyto metody ideálně ležely v hlavní nadřazené třídě Controller)
 
+    // --- Pomocné metody pro systém notifikací ---
+
     protected function addSuccessMessage($message) {
-        // Zde by byla logika pro uložení zelené zprávy o úspěchu (např. do $_SESSION)
+        // Zelená zpráva o úspěchu
+        $_SESSION['messages']['success'][] = $message;
     }
 
     protected function addNoticeMessage($message) {
-        // Zde by byla logika pro uložení žluté informativní zprávy (např. do $_SESSION)
+        // Žlutá informativní zpráva
+        $_SESSION['messages']['notice'][] = $message;
     }
 
     protected function addErrorMessage($message) {
-        // Zde by byla logika pro uložení červené chybové zprávy (např. do $_SESSION)
+        // Červená chybová zpráva
+        $_SESSION['messages']['error'][] = $message;
     }
 
         // 3. Smazání existující knihy
@@ -224,4 +229,5 @@ class BookController {
             $this->addNoticeMessage('Pro úpravu knihy je nutné odeslat formulář.');
         }
     }
+       
 }
