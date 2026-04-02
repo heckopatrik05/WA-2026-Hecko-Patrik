8d529d30fc9dcd695aba55433e493565b33830eb
__hpatrik__ <heckopatrik05@gmail.com>
2026-04-02 11:40:56 +0200

    funguje

--- BooksApp/App/Views/Books/book_create.php
+++ BooksApp/App/Views/Books/book_create.php
@@ -14,7 +14,7 @@
         </div>
         
         <div>
-            <form action="/WA-2026-Hecko-Patrik/BooksApp/public/index.php?url=book/store" method="post" enctype="multipart/form-data">
+            <form action="<?= BASE_URL ?>/index.php?url=book/store" method="post" enctype="multipart/form-data">
                 <div>
                     <div>
                         <label for="title">Název knihy <span>*</span></label>
